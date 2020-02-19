47de107024f494225885d9b83471af4836723c59
huangjianBJB <13830740980123852@163.com>
2020-02-19 17:16:30 +0800

    hj

---
+++ jeecg-boot/jeecg-boot-module-sj_lstj/src/main/java/org/jeecg/boot/module/sj_lstj/App.java
@@ -0,0 +1,13 @@
+package org.jeecg.boot.module.sj_lstj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
